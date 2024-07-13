99de2b7d51744820e5cc59dacf7fb80d8d36a2b3
PrabhavStudent <prabhavnirwal01@gmail.com>
2024-07-13 17:22:49 +0530

    home page background

--- hotel-reservation/src/components/Home.jsx
+++ hotel-reservation/src/components/Home.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './Home.css';
 
-const Home = () => {
+const Home = ({ setView }) => {
+  const navigateToRooms = () => {
+    console.log('Navigating to Rooms view');
+    setView('rooms');
+  };
+
   return (
     <div className="home-container" style={{
-      backgroundImage:"url(/images/hotel.jpg)",height : "100vh",backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundPosition:"center"
+      backgroundImage: "url(/images/hotel.jpg)",
+      height: "100vh",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundPosition: "center"
     }}>
       <h1>Welcome to Our Hotel</h1>
       <p>Experience the best stay with us.</p>
-      
+      <div className="to-room">
+        <button onClick={navigateToRooms} className="button">Rooms</button>
+      </div>
     </div>
   );
 };
